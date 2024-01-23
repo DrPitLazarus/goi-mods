8a9bb51c96c77213d72000385848ae77931b8dd5
Pit <drpitlazarus@gmail.com>
2024-01-22 20:34:14 -0500

    SkipLauncherAndIntro v1.0.0 init

---
+++ SkipLauncherAndIntro/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+
+namespace SkipLauncherAndIntro
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(UILauncherMainPanel), "Activate")]
+        [HarmonyPrefix]
+        public static bool SkipLauncherAndIntro()
+        {
+            // Use reflection to call a private method
+            var func = typeof(UILauncherMainPanel).GetMethod("Deactivate", BindingFlags.Instance | BindingFlags.NonPublic);
+            func.Invoke(new UILauncherMainPanel(), null);
+
+            // Don't run the original method
+            return false;
+        }
+    }
+}
