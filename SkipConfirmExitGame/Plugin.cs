e0da48ce764c846873fb22620f8b8ac3ef6aad3d
Pit <drpitlazarus@gmail.com>
2024-01-24 20:49:34 -0500

    SkipConfirmExitGame v1.0.0 init

---
+++ SkipConfirmExitGame/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MuseBase.Multiplayer.Unity;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SkipConfirmExitGame
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch]
+    class Patch
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            var type = AccessTools.FirstInner(typeof(UIManager), t => t.Name.Contains("UINewMainMenuState"));
+            var method = AccessTools.FirstMethod(type, method => method.Name.Contains("ExitGame"));
+            yield return method;
+            // Maybe exit confirmation is ok in match menu...
+            // type = AccessTools.FirstInner(typeof(UIManager), t => t.Name.Contains("UIMatchMenuState"));
+            // method = AccessTools.FirstMethod(type, method => method.Name.Contains("ExitGame"));
+            // yield return method;
+        }
+
+        static bool Prefix()
+        {
+            // Who named this? I think it's safer than calling Application.Quit()... Maybe.
+            UITextDrone.PumpAndDie();
+            // Don't run the original method
+            return false;
+        }
+    }
+
+    [HarmonyPatch]
+    class PatchConnectingScreen
+    {
+        static MethodBase TargetMethod()
+        {
+            var method = AccessTools.FirstMethod(typeof(UIConnectingScreen), method => method.Name.Contains("Awake"));
+            return method;
+        }
+
+        static bool Prefix(UIConnectingScreen __instance)
+        {
+            // Remove modal
+            __instance.exitGameButton.onClick.AddListener(delegate ()
+            {
+                // Who named this? I think it's safer than calling Application.Quit()... Maybe.
+                UITextDrone.PumpAndDie();
+            });
+            // Keep original method
+            __instance.returnToLobbyButton.onClick.AddListener(delegate ()
+            {
+                MuseWorldClient.Instance.ReconnectToLobby();
+            });
+            // Don't run the original method
+            return false;
+        }
+    }
+}
---
+++ SkipConfirmExitGame/readme.md
@@ -0,0 +1,12 @@
+# SkipConfirmExitGame
+
+[Downloads](https://github.com/DrPitLazarus/goi-mods/releases/tag/SkipConfirmExitGame) -
+[Directions](/readme.md)
+
+A quality of life mod for the game Guns of Icarus Online/Alliance.
+
+Removes the confirm screen when clicking the exit button on the connecting screen and main menu.
+
+The exit button while in a match still has the confirm screen.
+
+![screenshot](screenshot.webp)
